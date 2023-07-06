56ba168871cfa60be3b30d178ef55e8a762a1e56
Rémy Mondi <remy@mondi.dev>
2023-07-06 13:16:34 +0200

    [Update] Sitemap query

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -43,7 +43,7 @@ export const getServerSideProps = async ({ res }) => {
 
   const reference_query = `
     query {
-      references {
+      references (pagination: {start: 0, limit: -1}) {
         data {
           attributes {
             slug
